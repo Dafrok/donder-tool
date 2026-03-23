3b2cc7929784ea85c16fedc07fab2615a9f032b4
tapu-gugu <415660840@qq.com>
2026-03-23 23:45:48 +0800

    test

--- src/assets/py/复合.py
+++ src/assets/py/复合.py
@@ -140,8 +140,8 @@ def calculate_compound_difficulty(an: List[float], bn: List[int], T: float) -> f
             # 当前元素的标记必须为"L"才进行详细计算
             if current_lr_mark == 'L':
                 # 2. 计算阈值边界
-                lower_limit = 15/300 * 1000  # 约为 50.0
-                upper_limit = 15/220 * 1000  # 约为 68.18
+                lower_limit = 30/300 * 1000  # 300bpm的8分音
+                upper_limit = 30/180 * 1000  # 180bpm的8分音
                 value_to_check = an_extended[idx_i-1]  # 当前元素前一个位置的值
                 
                 # 3. 根据 value_to_check 与阈值的关系计算c
